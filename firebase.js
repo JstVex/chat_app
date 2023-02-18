558b5fb380db9cc8d6acbea667e969ce5d5afccf
JstVex <heinhtetlulu14@gmail.com>
2023-02-18 12:25:04 +0630

    Initial commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBebr1AG3L0s4f-DvOI6U7CtFSy5NcFXYs",
+    authDomain: "chatapp-375f8.firebaseapp.com",
+    projectId: "chatapp-375f8",
+    storageBucket: "chatapp-375f8.appspot.com",
+    messagingSenderId: "144460943456",
+    appId: "1:144460943456:web:02983e46f8541e54fcb305"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
